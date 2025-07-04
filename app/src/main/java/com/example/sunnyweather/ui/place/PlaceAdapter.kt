cb751ade7b4b2270214f286f43b6473983c209ce
lzq <lzq08lucky@163.com>
2025-07-04 20:38:11 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceAdapter.kt
@@ -1,16 +1,19 @@
 package com.example.sunnyweather.ui.place
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sunnyweather.MainActivity
 import com.example.sunnyweather.R
 import com.example.sunnyweather.logic.model.Place
+import com.example.sunnyweather.ui.weather.WeatherActivity
 
 class PlaceAdapter(private val fragment: PlaceFragment, private val list: List<Place>) :
     RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
-    inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
+    inner class  ViewHolder(view : View) : RecyclerView.ViewHolder(view){
         val placeName : TextView = view.findViewById<TextView>(R.id.placeName)
         val placeAddress : TextView = view.findViewById<TextView>(R.id.placeAddress)
     }
@@ -21,7 +24,32 @@ class PlaceAdapter(private val fragment: PlaceFragment, private val list: List<P
     ): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.place_item,
             parent, false)
-        return ViewHolder(view)
+        val holder = ViewHolder(view)
+        holder.itemView.setOnClickListener {
+            val position = holder.adapterPosition
+            val place = list[position]
+            val activity = fragment.activity
+            if(activity is WeatherActivity){
+
+                activity.drawerLayout.closeDrawers()
+                activity.viewModel.locationLng = place.location.lng
+                activity.viewModel.locationLat = place.location.lat
+                activity.viewModel.placeName = place.name
+                activity.refreshWeather()
+            }else{
+                val intent = Intent(parent.context, WeatherActivity::class.java).apply {
+                    putExtra("location_lng", place.location.lng)
+                    putExtra("location_lat", place.location.lat)
+                    putExtra("place_name", place.name)
+                }
+                fragment.startActivity(intent)
+                fragment.activity?.finish()
+            }
+
+            fragment.viewModel.savePlace(place)
+
+        }
+        return holder
     }
 
     override fun onBindViewHolder(
--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
@@ -1,5 +1,6 @@
 package com.example.sunnyweather.ui.place
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,7 +14,10 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sunnyweather.MainActivity
 import com.example.sunnyweather.R
+import com.example.sunnyweather.logic.model.Weather
+import com.example.sunnyweather.ui.weather.WeatherActivity
 
 class PlaceFragment : Fragment() {
     val viewModel by lazy {
@@ -30,10 +34,20 @@ class PlaceFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        if(activity is MainActivity && viewModel.isPlaceSaved()){
+            val place = viewModel.getSavedPlace()
+            val intent  = Intent(context, WeatherActivity::class.java).apply {
+                putExtra("location_lng", place.location.lng)
+                putExtra("location_lat", place.location.lat)
+                putExtra("place_name", place.name)
+            }
+            startActivity(intent)
+            activity?.finish()
+            return
+        }
         val layoutManager = LinearLayoutManager(activity)
         val recyclerView = requireView().findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = layoutManager
-
         adapter = PlaceAdapter(this, viewModel.placeList)
         recyclerView.adapter = adapter
         val searchPlaceEdit = requireView().findViewById<EditText>(R.id.searchPlaceEdit)
